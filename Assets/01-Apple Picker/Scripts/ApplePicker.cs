2d17f4920489fd473b6e753ee124e1c6922360ae
2ne1WonderGirls <47704462+2ne1WonderGirls@users.noreply.github.com>
2021-09-17 01:32:30 -0400

    Destroy Basket
    Destroy basket

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;                                         // b
 
 
 
@@ -12,6 +13,7 @@ public class ApplePicker : MonoBehaviour {
     public int              numBaskets = 3;
     public float            basketBottomY = -14f;
     public float            basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
 
 
@@ -22,6 +24,30 @@ public class ApplePicker : MonoBehaviour {
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + ( basketSpacingY * i );
             tBasketGO.transform.position = pos;
+            basketList.Add( tBasketGO );                                   // d
         }
     }
+
+    
+    public void AppleDestroyed() {
+
+        // Destroy all of the falling apples
+        GameObject[] tAppleArray=GameObject.FindGameObjectsWithTag("Apple");
+
+        foreach ( GameObject tGO in tAppleArray ) {
+            Destroy( tGO );
+        }
+
+        // Destroy one of the baskets                                      // e
+        // Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count-1;
+
+        // Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+
+        // Remove the Basket from the list and destroy the GameObject
+        basketList.RemoveAt( basketIndex );
+        Destroy( tBasketGO );
+
+    } 
 }
