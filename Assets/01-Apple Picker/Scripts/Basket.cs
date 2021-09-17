f6b68b5bc16735b1d3663cffe0c2e8edd402dca4
2ne1WonderGirls <47704462+2ne1WonderGirls@users.noreply.github.com>
2021-09-17 01:09:59 -0400

    UI
    High Score counter

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;  
 
 
 public class Basket : MonoBehaviour {
+
+  [Header("Set Dynamically")]
+    public Text           scoreGT;                                          // a
+
+     void Start() {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");               // b
+
+        // Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();                             // c
+
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
 
     void Update () {
 
@@ -30,6 +44,16 @@ public class Basket : MonoBehaviour {
 
         if ( collidedWith.tag == "Apple" ) {                          // c
             Destroy( collidedWith );
+
+         // Parse the text of the scoreGT into an int
+            int score = int.Parse( scoreGT.text );                          // d
+
+            // Add points for catching the apple
+            score += 100;
+
+            // Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
         }
+
      }
 }
