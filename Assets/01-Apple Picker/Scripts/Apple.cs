ad3484c7fd81d8273a176435a42fe40c68c02ea6
2ne1WonderGirls <47704462+2ne1WonderGirls@users.noreply.github.com>
2021-09-17 01:13:02 -0400

    AppleDestroyed
    AppleDestroyed script

--- Assets/01-Apple Picker/Scripts/Apple.cs
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -9,6 +9,21 @@ public class Apple : MonoBehaviour {
 
         if ( transform.position.y < bottomY ) {
             Destroy( this.gameObject );                                      // b
+            
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();  // b
+
+            // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed();                                       // c
+        }
+    }
+       public void AppleDestroyed() {                                           // a
+
+        // Destroy all of the falling apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple"); // b
+
+        foreach ( GameObject tGO in tAppleArray ) {
+            Destroy( tGO );
         }
     }
 }           
