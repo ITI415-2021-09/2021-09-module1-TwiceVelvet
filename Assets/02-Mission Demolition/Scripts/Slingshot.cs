a2d186b1bb5091d785bf893605d5961a2347580f
2ne1WonderGirls <47704462+2ne1WonderGirls@users.noreply.github.com>
2021-09-21 02:26:16 -0400

    Slingshot code
    Scripting a launch point

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+public GameObject         launchPoint;
+
+    void Awake() {
+        Transform launchPointTrans = transform.Find("LaunchPoint");              // a
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive( false );                                          // b
+    }
+
+    void OnMouseEnter() {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive( true );                                           // b
+    }
+
+    void OnMouseExit() {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive( false );            
+    }
+
+
+}
