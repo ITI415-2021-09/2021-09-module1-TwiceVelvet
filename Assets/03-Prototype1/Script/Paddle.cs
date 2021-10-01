e5ea41ccf45148261b9d86ccf151bb9e4e755732
2ne1WonderGirls <47704462+2ne1WonderGirls@users.noreply.github.com>
2021-10-01 00:51:25 -0400

    Paddle Script
    Make it so that the paddle can go from edge to edge without overlapping the walls.

--- Assets/03-Prototype1/Script/Paddle.cs
+++ Assets/03-Prototype1/Script/Paddle.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    public Camera mainCamera;
+    float leftMax = 70;
+    float rightMax = 530;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = FindObjectOfType<Camera>(); //setting main camera
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3(Input.mousePosition.x,0,0);
+        Movement();
+        
+    }
+
+    void Movement(){
+        float maxMovement = Mathf.Clamp(Input.mousePosition.x,leftMax,rightMax);
+        float worldxPos = mainCamera.ScreenToWorldPoint(new Vector3(maxMovement,0,0)).x;
+        //Paddle moves w/ mouse in relation to main camera
+        this.transform.position = new Vector3(worldxPos, -4, 0);
     }
 }
